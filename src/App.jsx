9ac5a9e85b929b95a7b4e23fb772e62cab1c0921
Alex Gievsky <alex.gievsky@gmail.com>
2024-07-31 12:56:54 +0200

    Update styles.

--- src/App.jsx
+++ src/App.jsx
@@ -230,302 +230,297 @@ import Breadcrumbs from './Breadcrumbs';
 import './App.css';
 
 const branches = {
-  0: {
-    name: "/",
-    items: [
-      { id: 1, created_at: new Date(), name: "1", ext: null },
-      { id: 2, created_at: new Date(), name: "2", ext: null },
-      { id: 3, created_at: new Date(), name: "3", ext: null },
-      { id: 4, created_at: new Date(), name: "4", ext: "txt" },
-      { id: 5, created_at: new Date(), name: "5", ext: "jpg" },
-      { id: 6, created_at: new Date(), name: "6", ext: "zip" },
-      { id: 7, created_at: new Date(), name: "7", ext: "pdf" },
-      { id: 8, created_at: new Date(), name: "8", ext: "rar" },
-    ],
-    breadcrumbs: [
-    ]
-  },
-  1: {
-    name: "1",
-    items: [
-      { id: 11, created_at: new Date(), name: "11", ext: null },
-      { id: 12, created_at: new Date(), name: "12", ext: "mov" },
-      { id: 13, created_at: new Date(), name: "13", ext: "doc" },
-      { id: 14, created_at: new Date(), name: "14", ext: "mp4" },
-    ],
-    breadcrumbs: [
-      { id: 0, name: "/" }
-    ]
-  },
-  11: {
-    name: "11",
-    items: [
-      { id: 111, created_at: new Date(), name: "111", ext: "pdf" },
-      { id: 112, created_at: new Date(), name: "112", ext: "txt" },
-    ],
-    breadcrumbs: [
-      { id: 1, name: "1" },
-      { id: 0, name: "/" }
-    ]
-  },
-  2: {
-    name: "2",
-    items: [
-      { id: 21, created_at: new Date(), name: "21", ext: "pdf" },
-      { id: 22, created_at: new Date(), name: "22", ext: null },
-    ],
-    breadcrumbs: [
-      { id: 0, name: "/" }
-    ]
-  },
-  22: {
-    name: "22",
-    items: [
-      { id: 221, created_at: new Date(), name: "221", ext: null },
-      { id: 222, created_at: new Date(), name: "222", ext: null },
-    ],
-    breadcrumbs: [
-      { id: 2, name: "2" },
-      { id: 0, name: "/" }
-    ]
-  },
-  221: {
-    name: "221",
-    items: [
-    ],
-    breadcrumbs: [
-      { id: 22, name: "22" },
-      { id: 2, name: "2" },
-      { id: 0, name: "/" }
-    ]
-  },
-  222: {
-    name: "222",
-    items: [
-      { id: 2221, created_at: new Date(), name: "2221", ext: "png" },
-      { id: 2222, created_at: new Date(), name: "2222", ext: "mp4" },
-      { id: 2223, created_at: new Date(), name: "2222", ext: "dmg" },
-    ],
-    breadcrumbs: [
-      { id: 22, name: "22" },
-      { id: 2, name: "2" },
-      { id: 0, name: "/" }
-    ]
-  },
-  3: {
-    name: "3",
-    items: [
-      { id: 31, created_at: new Date(), name: "31", ext: "docx" },
-      { id: 32, created_at: new Date(), name: "32", ext: "txt" },
-      { id: 33, created_at: new Date(), name: "33", ext: null },
-    ],
-    breadcrumbs: [
-      { id: 0, name: "/" }
-    ]
-  },
-  33: {
-    name: "33",
-    items: [
-      { id: 331, created_at: new Date(), name: "331", ext: "docx" },
-      { id: 332, created_at: new Date(), name: "332", ext: "doc" },
-    ],
-    breadcrumbs: [
-      { id: 3, name: "3" },
-      { id: 0, name: "/" }
-    ]
-  },
+	0: {
+		name: "/",
+		items: [
+			{ id: 1, created_at: new Date(), name: "1", ext: null },
+			{ id: 2, created_at: new Date(), name: "2", ext: null },
+			{ id: 3, created_at: new Date(), name: "3", ext: null },
+			{ id: 4, created_at: new Date(), name: "4", ext: "txt" },
+			{ id: 5, created_at: new Date(), name: "5", ext: "jpg" },
+			{ id: 6, created_at: new Date(), name: "6", ext: "zip" },
+			{ id: 7, created_at: new Date(), name: "7", ext: "pdf" },
+			{ id: 8, created_at: new Date(), name: "8", ext: "rar" },
+		],
+		breadcrumbs: [
+		]
+	},
+	1: {
+		name: "1",
+		items: [
+			{ id: 11, created_at: new Date(), name: "11", ext: null },
+			{ id: 12, created_at: new Date(), name: "12", ext: "mov" },
+			{ id: 13, created_at: new Date(), name: "13", ext: "doc" },
+			{ id: 14, created_at: new Date(), name: "14", ext: "mp4" },
+		],
+		breadcrumbs: [
+			{ id: 0, name: "/" }
+		]
+	},
+	11: {
+		name: "11",
+		items: [
+			{ id: 111, created_at: new Date(), name: "111", ext: "pdf" },
+			{ id: 112, created_at: new Date(), name: "112", ext: "txt" },
+		],
+		breadcrumbs: [
+			{ id: 1, name: "1" },
+			{ id: 0, name: "/" }
+		]
+	},
+	2: {
+		name: "2",
+		items: [
+			{ id: 21, created_at: new Date(), name: "21", ext: "pdf" },
+			{ id: 22, created_at: new Date(), name: "22", ext: null },
+		],
+		breadcrumbs: [
+			{ id: 0, name: "/" }
+		]
+	},
+	22: {
+		name: "22",
+		items: [
+			{ id: 221, created_at: new Date(), name: "221", ext: null },
+			{ id: 222, created_at: new Date(), name: "222", ext: null },
+		],
+		breadcrumbs: [
+			{ id: 2, name: "2" },
+			{ id: 0, name: "/" }
+		]
+	},
+	221: {
+		name: "221",
+		items: [
+		],
+		breadcrumbs: [
+			{ id: 22, name: "22" },
+			{ id: 2, name: "2" },
+			{ id: 0, name: "/" }
+		]
+	},
+	222: {
+		name: "222",
+		items: [
+			{ id: 2221, created_at: new Date(), name: "2221", ext: "png" },
+			{ id: 2222, created_at: new Date(), name: "2222", ext: "mp4" },
+			{ id: 2223, created_at: new Date(), name: "2222", ext: "dmg" },
+		],
+		breadcrumbs: [
+			{ id: 22, name: "22" },
+			{ id: 2, name: "2" },
+			{ id: 0, name: "/" }
+		]
+	},
+	3: {
+		name: "3",
+		items: [
+			{ id: 31, created_at: new Date(), name: "31", ext: "docx" },
+			{ id: 32, created_at: new Date(), name: "32", ext: "txt" },
+			{ id: 33, created_at: new Date(), name: "33", ext: null },
+		],
+		breadcrumbs: [
+			{ id: 0, name: "/" }
+		]
+	},
+	33: {
+		name: "33",
+		items: [
+			{ id: 331, created_at: new Date(), name: "331", ext: "docx" },
+			{ id: 332, created_at: new Date(), name: "332", ext: "doc" },
+		],
+		breadcrumbs: [
+			{ id: 3, name: "3" },
+			{ id: 0, name: "/" }
+		]
+	},
 }
 
 export const openDir = (id) => {
-  console.log(`Opening directory: ${id}`);
-  return branches[id]
+	console.log(`Opening directory: ${id}`);
+	return branches[id]
 };
 
 export const openFile = (id) => {
-  console.log(`Opening file: ${id}`);
+	console.log(`Opening file: ${id}`);
 };
 
 const getIcon = (ext) => {
-  if (ext === null) {
-    return <FaFolder className="folder-icon" />;
-  }
-  switch (ext) {
-    case 'doc':
-    case 'docx':
-      return <FaFileWord className="word-icon" />;
-    case 'pdf':
-      return <FaFilePdf className="pdf-icon" />;
-    case 'jpg':
-    case 'jpeg':
-    case 'png':
-      return <FaFileImage className="image-icon" />;
-    case 'mp4':
-    case 'mov':
-      return <FaFileVideo className="video-icon" />;
-    case 'zip':
-    case 'rar':
-      return <FaFileArchive className="archive-icon" />;
-    default:
-      return <FaFile className="file-icon" />;
-  }
+	if (ext === null) {
+		return <FaFolder className="folder-icon" />;
+	}
+	switch (ext) {
+		case 'doc':
+		case 'docx':
+			return <FaFileWord className="word-icon" />;
+		case 'pdf':
+			return <FaFilePdf className="pdf-icon" />;
+		case 'jpg':
+		case 'jpeg':
+		case 'png':
+			return <FaFileImage className="image-icon" />;
+		case 'mp4':
+		case 'mov':
+			return <FaFileVideo className="video-icon" />;
+		case 'zip':
+		case 'rar':
+			return <FaFileArchive className="archive-icon" />;
+		default:
+			return <FaFile className="file-icon" />;
+	}
 };
 
 const App = () => {
-  const [dragActive, setDragActive] = useState(false);
-  const [dragCounter, setDragCounter] = useState(0);
-  const [currentDir, setCurrentDir] = useState(branches[0]);
+	const [dragActive, setDragActive] = useState(false);
+	const [dragCounter, setDragCounter] = useState(0);
+	const [currentDir, setCurrentDir] = useState(branches[0]);
 
-  const handleItemClick = (item) => {
-    if (item.ext === null) {
-      const newDir = openDir(item.id);
-      setCurrentDir(newDir);
-    } else {
-      openFile(item.id);
-    }
-  };
+	const handleItemClick = (item) => {
+		if (item.ext === null) {
+			const newDir = openDir(item.id);
+			setCurrentDir(newDir);
+		} else {
+			openFile(item.id);
+		}
+	};
 
-  const handleBackClick = () => {
-    let dir = branches[currentDir.breadcrumbs[0].id]
+	const handleBackClick = () => {
+		let dir = branches[currentDir.breadcrumbs[0].id]
 
-    setCurrentDir(dir)
-  };
+		setCurrentDir(dir)
+	};
 
-  const handleBreadcrumbClick = (id) => {
-    setCurrentDir(branches[id]);
-  };
+	const handleBreadcrumbClick = (id) => {
+		setCurrentDir(branches[id]);
+	};
 
-  const handleUpload = () => {
-    console.log("Upload File Clicked");
-  };
+	const handleUpload = () => {
+		console.log("Upload File Clicked");
+	};
 
-  const handleDragEnter = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragCounter(prev => prev + 1);
-    setDragActive(true);
-  };
+	const handleDragEnter = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		setDragCounter(prev => prev + 1);
+		setDragActive(true);
+	};
 
-  const handleDragLeave = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragCounter(prev => prev - 1);
-    if (dragCounter === 1) {
-      setDragActive(false);
-    }
-  };
+	const handleDragLeave = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		setDragCounter(prev => prev - 1);
+		if (dragCounter === 1) {
+			setDragActive(false);
+		}
+	};
 
-  const handleDragOver = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-  };
+	const handleDragOver = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+	};
 
-  const handleDrop = async (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    setDragActive(false);
-    setDragCounter(0);
+	const handleDrop = async (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		setDragActive(false);
+		setDragCounter(0);
 
-    const droppedItems = e.dataTransfer.items;
+		const droppedItems = e.dataTransfer.items;
 
-    const logNode = async (entry, path = "") => {
-      console.log(path + entry.name + (entry.isDirectory ? '/' : ''));
+		const logNode = async (entry, path = "") => {
+			console.log(path + entry.name + (entry.isDirectory ? '/' : ''));
 
-      if (entry.isDirectory) {
-        const reader = entry.createReader();
-        const readAllEntries = async () => {
-          let entries = [];
-          let readEntries;
-          do {
-            readEntries = await new Promise((resolve) => reader.readEntries(resolve));
-            entries = entries.concat(readEntries);
-          } while (readEntries.length > 0);
-          return entries;
-        };
+			if (entry.isDirectory) {
+				const reader = entry.createReader();
+				const readAllEntries = async () => {
+					let entries = [];
+					let readEntries;
+					do {
+						readEntries = await new Promise((resolve) => reader.readEntries(resolve));
+						entries = entries.concat(readEntries);
+					} while (readEntries.length > 0);
+					return entries;
+				};
 
-        const entries = await readAllEntries();
-        for (let i = 0; i < entries.length; i++) {
-          await logNode(entries[i], path + entry.name + '/');
-        }
-      }
-    };
+				const entries = await readAllEntries();
+				for (let i = 0; i < entries.length; i++) {
+					await logNode(entries[i], path + entry.name + '/');
+				}
+			}
+		};
 
-    for (let i = 0; i < droppedItems.length; i++) {
-      const item = droppedItems[i].webkitGetAsEntry();
-      if (item) {
-        await logNode(item);
-      }
-    }
-  };
+		for (let i = 0; i < droppedItems.length; i++) {
+			const item = droppedItems[i].webkitGetAsEntry();
+			if (item) {
+				await logNode(item);
+			}
+		}
+	};
 
-  useEffect(() => {
-    window.addEventListener('dragenter', handleDragEnter);
-    window.addEventListener('dragleave', handleDragLeave);
-    window.addEventListener('dragover', handleDragOver);
-    window.addEventListener('drop', handleDrop);
+	useEffect(() => {
+		window.addEventListener('dragenter', handleDragEnter);
+		window.addEventListener('dragleave', handleDragLeave);
+		window.addEventListener('dragover', handleDragOver);
+		window.addEventListener('drop', handleDrop);
 
-    return () => {
-      window.removeEventListener('dragenter', handleDragEnter);
-      window.removeEventListener('dragleave', handleDragLeave);
-      window.removeEventListener('dragover', handleDragOver);
-      window.removeEventListener('drop', handleDrop);
-    };
-  }, [dragCounter]);
+		return () => {
+			window.removeEventListener('dragenter', handleDragEnter);
+			window.removeEventListener('dragleave', handleDragLeave);
+			window.removeEventListener('dragover', handleDragOver);
+			window.removeEventListener('drop', handleDrop);
+		};
+	}, [dragCounter]);
 
-  return (
-    <div className="app">
-      <div className="header">
-        <div>
-          {/* <h1>Files</h1> */}
-          {/* <p>Upload and manage files and playbooks necessary for facilitating your response to incidents.</p> */}
-        </div>
-        {/* <button className="upload-button" onClick={handleUpload}>Upload File</button> */}
-      </div>
-      <div className="table-container">
-        <table className="file-table">
-          <thead>
-            <Breadcrumbs
-              breadcrumbs={currentDir.breadcrumbs}
-              currentDirName={currentDir.name}
-              onBreadcrumbClick={handleBreadcrumbClick}
-              onUploadClick={handleUpload}
-            />
-            <tr>
-              <th>Name</th>
-              <th>Created At</th>
-              <th>Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentDir.breadcrumbs.length !== 0 && (
-              <tr key="back" onClick={handleBackClick}>
-                <td colSpan="3">
-                  <span className="file-icon">{getIcon(null)}</span>
-                  {".."}
-                </td>
-              </tr>
-            )}
-            {currentDir.items.map((item, index) => (
-              <tr key={index} onClick={() => handleItemClick(item)}>
-                <td>
-                  <span className="file-icon">{getIcon(item.ext)}</span>
-                  {item.name}
-                </td>
-                <td>{item.created_at.toLocaleString()}</td>
-                <td>{item.ext ?? "dir"}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {dragActive && (
-        <div className="drop-zone-overlay">
-          <div className="drop-zone">
-            <p>Drop files here</p>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+	return (
+		<div className="app">
+			<div className="header">
+			</div>
+			<div className="table-container">
+				<table className="file-table">
+					<thead>
+						<Breadcrumbs
+							breadcrumbs={currentDir.breadcrumbs}
+							currentDirName={currentDir.name}
+							onBreadcrumbClick={handleBreadcrumbClick}
+							onUploadClick={handleUpload}
+						/>
+						<tr>
+							<th className="name-column">Name</th>
+							<th className="created-at-column">Created At</th>
+							<th className="type-column">Type</th>
+						</tr>
+					</thead>
+					<tbody>
+						{currentDir.breadcrumbs.length !== 0 && (
+							<tr key="back" onClick={handleBackClick}>
+								<td colSpan="3">
+									<span className="file-icon">{getIcon(null)}</span>
+									{".."}
+								</td>
+							</tr>
+						)}
+						{currentDir.items.map((item, index) => (
+							<tr key={index} onClick={() => handleItemClick(item)}>
+								<td>
+									<span className="file-icon">{getIcon(item.ext)}</span>
+									{item.name}
+								</td>
+								<td>{item.created_at.toLocaleString()}</td>
+								<td>{item.ext ?? "dir"}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+			{dragActive && (
+				<div className="drop-zone-overlay">
+					<div className="drop-zone">
+						<p>Drop files here</p>
+					</div>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default App;
--- src/Breadcrumbs.jsx
+++ src/Breadcrumbs.jsx
@@ -2,29 +2,28 @@ import React from 'react';
 import { FaChevronRight } from 'react-icons/fa';
 
 const Breadcrumbs = ({ breadcrumbs, currentDirName, onBreadcrumbClick, onUploadClick }) => {
-    return (
-        <tr>
-            <td colSpan="2" className="breadcrumbs-container">
-                <div className="breadcrumbs">
-                    {breadcrumbs.slice().reverse().map((breadcrumb, index) => (
-                        <span key={breadcrumb.id} className="breadcrumb-container">
-                            <span onClick={() => onBreadcrumbClick(breadcrumb.id)} className="breadcrumb">
-                                {breadcrumb.name}
-                            </span>
-                            <FaChevronRight className="breadcrumb-delimiter" />
-                        </span>
-                    ))}
-                    <span className="breadcrumb-current-dir">{currentDirName}</span>
-                </div>
-            </td>
-						<td>
-
-                <div className="upload-button-container">
-                    <button className="upload-button" onClick={onUploadClick}>Upload File</button>
-                </div>
-						</td>
-        </tr>
-    );
+	return (
+		<tr>
+			<td colSpan="2">
+				<div className="breadcrumbs">
+					{breadcrumbs.slice().reverse().map((breadcrumb, index) => (
+						<span key={breadcrumb.id} className="breadcrumb-container">
+							<span onClick={() => onBreadcrumbClick(breadcrumb.id)} className="breadcrumb">
+								{breadcrumb.name}
+							</span>
+							<FaChevronRight className="breadcrumb-delimiter" />
+						</span>
+					))}
+					<span className="breadcrumb-current-dir">{currentDirName}</span>
+				</div>
+			</td>
+			<td>
+				<div className="upload-button-container">
+					<button className="upload-button" onClick={onUploadClick}>Upload File</button>
+				</div>
+			</td>
+		</tr>
+	);
 };
 
 export default Breadcrumbs;
